6962af9a19762e25de7568061f5e9db395c8b1e7
Raj Lad <raj95lad@gmail.com>
2019-03-14 07:00:05 +0700

    Flip the maxtrix

--- demo_scripts/heat_map_test.py
+++ demo_scripts/heat_map_test.py
@@ -9,22 +9,22 @@ import time
 
 actual_resolution = 'data/actual_resolution.txt'
 
-max_resolution = 'data/max_resolution.txt'
+max_resolution = 'data/on_startup_1/max_res.txt'
 
 
 
 def generate_heatmap(mat, i):
-    print(mat)
     heat_map = sns.heatmap(mat, annot=True)
     figure = heat_map.get_figure()
     figure.savefig('heat_map%s.png' % i)
     plt.clf()
-    # plt.show()
 
 
 def output_images(fname, m, n):
     fp = open(fname, 'r')
     for i, line in enumerate(fp):
+        if i < 99:
+            continue
         line = line.strip()
         start = 0
         end = n
@@ -37,7 +37,12 @@ def output_images(fname, m, n):
             start += 76 # n or 8 for actual
             end += 76 # n or 8 for actual
         np_mat_rep = np.array(mat_rep)
+        np_mat_rep = np.flipud(np_mat_rep)
+        np_mat_rep = np.fliplr(np_mat_rep)
+        print('matrix is' , np_mat_rep)
+        print('i is', i)
         generate_heatmap(np_mat_rep, i)
+        
     
     
     fp.close()
